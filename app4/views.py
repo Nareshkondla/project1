d2167cb19f5404c6957b2036ee2e74ee077f62ce
Nareshkondla <kondlanaresh3434@gmail.com>
2020-03-20 21:45:45 +0530

    Fisrt commit

---
+++ app4/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def naresh(request):
+    return HttpResponse("<H1>Sorry! Please Come to Game,If possible. By the way i am in home now. </h1>")
